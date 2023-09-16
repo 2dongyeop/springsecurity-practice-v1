67a729482e47b46870eec5ad7c6017c06bd30999
leedongyeop <ldy_1204@naver.com>
2023-09-16 15:03:44 +0900

    initial commit

---
+++ src/main/java/io/dongvelop/springsecuritypractice/SpringsecurityPracticeV1Application.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityPracticeV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityPracticeV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/dongvelop/springsecuritypractice/SpringsecurityPracticeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityPracticeV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
